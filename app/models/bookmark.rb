ebbbf37dcbf8fce6975a311162ec38191f5ce047
Claire Lin <chunhuilin7516@gmail.com>
2022-10-31 22:06:05 +0800

    add validation and associations to all three models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  # a movie can only show up once in every list
+  validates_uniqueness_of :movie, scope: :list_id
 end
