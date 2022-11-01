f570cb99cee136ec4988ca5b8df2ce502aaa2e63
Claire Lin <chunhuilin7516@gmail.com>
2022-11-01 17:19:59 +0800

    seed top rated movies on TMDB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# use JSON Formatter to display JSON nicely in your browser
+require "json"
+require "open-uri"
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movie_serialized = URI.open("#{url}?page=#{i + 1}".read)
+  movies = JSON.parse(movie_serialized)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie["title"],
+      overview: movie["overview"],
+      poster_url: "#{base_poster_url}#{movie["backdrop_path"]}",
+      rating: movie["vote_average"]
+    )
+  end
+end
+
+puts "Movie created"
