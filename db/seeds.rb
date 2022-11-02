77d1e8c3789846c90cc5f392530f1e237559537b
Claire Lin <chunhuilin7516@gmail.com>
2022-11-02 15:41:03 +0800

    add show method to movie controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,12 @@ movie_serialized = URI.open(url).read
 movies = JSON.parse(movie_serialized)['results']
 movies.each do |movie|
   puts "Creating #{movie['title']}"
-  base_poster_url = "https://image.tmdb.org/t/p/w500/original"
+
   Movie.create(
     title: movie["title"],
     overview: movie["overview"],
-    poster_url: "#{base_poster_url}#{movie["backdrop_path"]}",
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['backdrop_path']}",
     rating: movie["vote_average"]
   )
 end
