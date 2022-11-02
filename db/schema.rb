8c51302f36e642e3b778b8ae251ab05373c2b03f
Claire Lin <chunhuilin7516@gmail.com>
2022-11-02 16:16:10 +0800

    add release date to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_074424) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_081247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_074424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "backdrop_path"
+    t.date "release_date"
   end
 
   add_foreign_key "bookmarks", "lists"
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,12 +20,14 @@ movie_serialized = URI.open(url).read
 movies = JSON.parse(movie_serialized)['results']
 movies.each do |movie|
   puts "Creating #{movie['title']}"
+  base_poster_url = "https://image.tmdb.org/t/p/w500"
 
   Movie.create(
     title: movie["title"],
+    release_date: movie["release_date"],
     overview: movie["overview"],
-    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
-    # poster_url: "https://image.tmdb.org/t/p/w500#{movie['backdrop_path']}",
+    backdrop_path: "#{base_poster_url}#{movie['backdrop_path']}",
+    poster_url: "#{base_poster_url}#{movie['poster_path']}",
     rating: movie["vote_average"]
   )
 end
