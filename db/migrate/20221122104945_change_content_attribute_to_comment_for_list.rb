7130ab2098810b1d84afbf20e3f0a307cd3e3221
Claire Lin <chunhuilin7516@gmail.com>
2022-11-22 18:55:54 +0800

    Change attribute name of reviews from content to comment

---
+++ db/migrate/20221122104945_change_content_attribute_to_comment_for_list.rb
@@ -0,0 +1,5 @@
+class ChangeContentAttributeToCommentForList < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :reviews, :content, :comment
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_084342) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_104945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_084342) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.text "content"
+    t.text "comment"
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
